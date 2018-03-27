88007473866fb21e94daf0919766ab77a0956388
Tingchang Deng <dengtingchang@gmail.com>
2018-03-27 15:53:17 -0700

    create Page struct to insert into template

--- main.go
+++ main.go
@@ -6,11 +6,20 @@ import (
 	"html/template"
 )
 
+type Page struct {
+	Name string
+}
+
 func main() {
 	templates := template.Must(template.ParseFiles("templates/index.html"))
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if err := templates.ExecuteTemplate(w,"index.html", nil); err != nil {
+		p := Page{Name: "Gopher"}
+		if name := r.FormValue("name"); name != "" {
+			p.Name = name
+		}
+
+		if err := templates.ExecuteTemplate(w,"index.html", p); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 
