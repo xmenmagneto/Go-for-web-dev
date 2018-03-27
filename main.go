3b43f317a52b70bc5155c63fb8080f9f45e06afb
Tingchang Deng <dengtingchang@gmail.com>
2018-03-27 15:49:28 -0700

    parse index.html template

--- main.go
+++ main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"html/template"
 )
 
 func main() {
+	templates := template.Must(template.ParseFiles("templates/index.html"))
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, go web dev")
+		if err := templates.ExecuteTemplate(w,"index.html", nil); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
 	})
 
 	fmt.Println(http.ListenAndServe(":8080", nil))
